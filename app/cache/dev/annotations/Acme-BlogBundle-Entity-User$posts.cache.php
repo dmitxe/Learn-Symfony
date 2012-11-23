17b3fd108153436f2e814fe6111dda64c67b8f23
Smart-Core <artem.digi@gmail.com>
2012-11-23 15:24:17 +0700

    celan up

--- app/cache/dev/annotations/Acme-BlogBundle-Entity-User$posts.cache.php
+++
@@ -1,1 +0,0 @@
-<?php return unserialize('a:2:{i:0;O:30:"Doctrine\\ORM\\Mapping\\OneToMany":6:{s:8:"mappedBy";s:4:"user";s:12:"targetEntity";s:4:"Post";s:7:"cascade";N;s:5:"fetch";s:4:"LAZY";s:13:"orphanRemoval";b:0;s:7:"indexBy";N;}i:1;O:28:"Doctrine\\ORM\\Mapping\\OrderBy":1:{s:5:"value";a:1:{s:9:"createdAt";s:4:"DESC";}}}');
